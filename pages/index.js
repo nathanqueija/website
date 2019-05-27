88a34aa5c89c931e210d7809c2b47f66825cb7c1
Nathan Queija <nathanqcam@gmail.com>
2019-05-27 21:15:38 +0200

    Add custom routes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,22 @@
-function Home() {
-  return <div>Welcome to Next.js!</div>;
-}
+import React from 'react';
+import Link from 'next/link';
 
-export default Home;
+export default () => (
+  <ul>
+    <li>
+      <Link href="/b" as="/a">
+        <a>a</a>
+      </Link>
+    </li>
+    <li>
+      <Link href="/a" as="/b">
+        <a>b</a>
+      </Link>
+    </li>
+    <li>
+      <Link href={{ pathname: '/posts', query: { id: '2' } }} as="/posts/2">
+        <a>post #2</a>
+      </Link>
+    </li>
+  </ul>
+);
