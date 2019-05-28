a66b510cb3ed0b2bce88b73e5c86c1317c9b379b
Nathan Queija <nathanqcam@gmail.com>
2019-05-28 22:09:15 +0200

    Add favicons

--- pages/_app.js
+++ pages/_app.js
@@ -38,6 +38,24 @@ export default class MyApp extends App {
               href="https://fonts.googleapis.com/css?family=Libre+Baskerville:400,400i,700&display=swap"
               rel="stylesheet"
             />
+            <link
+              rel="apple-touch-icon"
+              sizes="180x180"
+              href="/static/favicon/apple-touch-icon.png"
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="32x32"
+              href="/static/favicon/favicon-32x32.png"
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="16x16"
+              href="/static/favicon/favicon-16x16.png"
+            />
+            <link rel="manifest" href="/static/favicon/site.webmanifest" />
           </Head>
           <GlobalStyles />
           <Layout>
