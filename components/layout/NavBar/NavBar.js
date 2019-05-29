2464067db20a004891e137f9d4fd847d68eb67b0
Nathan Queija <nathanqcam@gmail.com>
2019-05-29 13:32:19 +0200

    Fix routes

--- components/layout/NavBar/NavBar.js
+++ components/layout/NavBar/NavBar.js
@@ -2,14 +2,14 @@ import Nav from './NavBar.style';
 import NavLink from './NavLink';
 
 const menu = [
-  { path: '/', label: 'about-me' },
+  { path: '/about-me', label: 'about-me', as: '/' },
   { path: '/about-this', label: 'about-this-site' }
 ];
 const NavBar = () => (
   <Nav>
     <NavLink href='/b'>A</NavLink>
-    {menu.map(({ path, label }) => (
-      <NavLink key={path} href={path}>
+    {menu.map(({ path, label, as }) => (
+      <NavLink key={path} href={path} as={as}>
         ~/{label}
       </NavLink>
     ))}
--- components/layout/NavBar/NavLink/NavLink.js
+++ components/layout/NavBar/NavLink/NavLink.js
@@ -1,9 +1,12 @@
 import Link from 'next/link';
 import StyledLink from './NavLink.style';
 import { withRouter } from 'next/router';
-const NavLink = ({ href, children, router: { asPath } }) => (
-  <Link href={href} passHref>
-    <StyledLink active={href === asPath}>{children}</StyledLink>
+const NavLink = ({ href, children, as, router: { asPath } }) => (
+  <Link href={href} passHref as={as}>
+    <StyledLink active={href === asPath || as === asPath}>
+      {console.log(asPath, href)}
+      {children}
+    </StyledLink>
   </Link>
 );
 
